b49b313404aad2cc6bb1f60cc812ab353bb6400b
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-01-14 23:03:35 -0500

    Working on Password length Verification and Math.random for Password

--- script.js
+++ script.js
@@ -5,33 +5,52 @@ var generatePassword = function () {
   // criteria prompt for password length and verify between 8 and 128
   // Used Logical or || - learned from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR
 
-  // var passwordLength = prompt("Enter the password length:");
+  var passwordLength = prompt("Enter the password length:");
 
-  // if (passwordLength < 8 || passwordLength > 128 || isNan(passwordLength))
-   
-  //   alert("Please enter a number between 8 and 128.");
-  //   return;
+  // stuck on this part.  doesnt seem to be working (cant figure out isNaN and return)
+  if (passwordLength < 8 || passwordLength > 128 || Number.isNaN(passwordLength))
+    alert("Please enter a number between 8 and 128.");
+  // return;
 
-    // Vars with Prompts to confirm PW criteria of Upper case, Lower case, numbers and or special characters
+  // Vars with Prompts to confirm PW criteria of Upper case, Lower case, numbers and or special characters
 
-    var includeUppercase = prompt("Include Upper case characters?");
-    var includeLowercase = prompt("Include Lower Case characters?");
-    var includeSpecialChars = prompt("Include special characters?");
-    var includeNumbers = prompt("Include Numbers?");
+  var includeUppercase = confirm("Include Upper case characters?");
+  var includeLowercase = confirm("Include Lower Case characters?");
+  var includeSpecialChars = confirm("Include special characters?");
+  var includeNumbers = confirm("Include Numbers?");
 
-    // Vars with values fos Criterias
+  // Vars with values for Criterias
 
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var specialChar = "!@#$%^&*()-_=+<>?/[]{}";
-    var numberChar = "1234567890";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var specialChar = "!@#$%^&*()-_=+<>?/[]{}";
+  var numberChar = "1234567890";
 
-  Var AllChars = [upperCase , lowerCase, specialChar, numberChar];
+  // Having trouble with this (can this be done with a Var Array?)
+  var char = "";
+  if (includeUppercase) char += upperCase;
+  if (includeLowercase) char += lowerCase;
+  if (includeSpecialChars) char += specialChar;
+  if (includeNumbers) char += numberChar;
+
+  //verify at least one character set is included, Alert and return if not
+
+  if (char === "") {
+    alert("Please select at least one character type.");
+    return;
+  }
+
+  // var for password Having trouble figuring out how to use Math.random
 
 
+
+  var password =""
   
 
-  return "some password";
+
+
+
+  return password;
 }
 
 // Write password to the #password input
