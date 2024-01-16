7cfbb133fbd249526a54134d03bf51ece0af1672
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-01-16 13:36:06 -0500

    Corrected issue with PasswordLength criteria function and updated Readme

--- README.md
+++ README.md
@@ -7,13 +7,14 @@ Code Generator using JS script
 2. Open Index.html and style.css in you favorite code editor to view refactored code.
 
 Live site:
-TBD
+https://mojo718.github.io/PasswordGenerator
 
 
 Additional Resources i used for help:
 
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR
+https://www.w3schools.com/java/java_conditions.asp
 https://www.w3schools.com/js/js_loop_for.asp
 https://www.w3schools.com/java/java_for_loop.asp
 https://www.w3schools.com/jsref/jsref_random.asp
--- script.js
+++ script.js
@@ -5,51 +5,48 @@ var generatePassword = function () {
   // criteria prompt for password length and verify between 8 and 128
   // Used Logical or || - learned from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR
 
-  var passwordLength = prompt("Enter the password length:");
-
-  // stuck on this part.  doesnt seem to be working (cant figure out isNaN and return)
-  if (passwordLength < 8 || passwordLength > 128 || Number.isNaN(passwordLength))
-    alert("Please enter a number between 8 and 128.");
-  // return;
-
-  // Vars with Prompts to confirm PW criteria of Upper case, Lower case, numbers and or special characters
-
-  var includeUppercase = confirm("Include Upper case characters?");
-  var includeLowercase = confirm("Include Lower Case characters?");
-  var includeSpecialChars = confirm("Include special characters?");
-  var includeNumbers = confirm("Include Numbers?");
-
-  // Vars with values for Criterias
-
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var specialChar = "!@#$%^&*()-_=+<>?/[]{}";
-  var numberChar = "1234567890";
-
-  // Having trouble with this (can this be done with a Var Array?)
-  var char = "";
-  if (includeUppercase) char += upperCase;
-  if (includeLowercase) char += lowerCase;
-  if (includeSpecialChars) char += specialChar;
-  if (includeNumbers) char += numberChar;
-
-  //verify at least one character set is included, Alert and return if not
-
-  if (char === "") {
-    alert("Please select at least one character type.");
-    return;
+  var passwordLength = prompt("Enter the password length:"); 
+  // stuck on this part.  doesnt seem to be working (cant figure out if statement wont return)
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a number between 8 and 128 and try again.");
+   return;
   }
 
-  // var for password 
-  // Used for loop and math.random to generate password
+// Vars with Prompts to confirm PW criteria of Upper case, Lower case, numbers and or special characters
+var includeUppercase = confirm("Include Upper case characters?");
+var includeLowercase = confirm("Include Lower Case characters?");
+var includeSpecialChars = confirm("Include special characters?");
+var includeNumbers = confirm("Include Numbers?");
 
-  var password ="";
-  for (var i= 0; i < passwordLength; i++) 
-  {var randomIndex = Math.floor (Math.random() * char.length);
-    password +=char.charAt(randomIndex)
-  }
-  
-  return password;
+// Vars with values for Criterias
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!@#$%^&*()-_=+<>?/[]{}";
+var numberChar = "1234567890";
+
+// create a char variable that will append itself using the += operator 
+var char = "";
+if (includeUppercase) char += upperCase;
+if (includeLowercase) char += lowerCase;
+if (includeSpecialChars) char += specialChar;
+if (includeNumbers) char += numberChar;
+
+//verify at least one character set is included in char variable, if not, Alert and return 
+if (char === "") {
+  alert("Please select at least one character type.");
+  return;
+}
+
+// var for password 
+// Used for loop and math.random to generate password
+
+var password = "";
+for (var i = 0; i < passwordLength; i++) {
+  var randomPass = Math.floor(Math.random() * char.length);
+  password += char.charAt(randomPass)
+}
+
+return password;
 }
 
 // Write password to the #password input
@@ -60,6 +57,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
